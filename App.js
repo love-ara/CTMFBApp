c7803bbb1973f8fd46521c1a63527ef3afcb2d81
Aramide <aramideotenaike@gmail.com>
2024-07-23 06:12:48 +0100

    done: login page

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image, Alert, Button, SafeAreaView, Platform} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff ',
+    paddingTop: Platform.OS ==="android"? StatusBar.currentHeight : 0,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#208220',
+  }
 });
